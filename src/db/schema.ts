389ba2f260917ca9e5dbe4a76fbbc5bb72f9c96f
David Eastmond <davebuck1981@gmail.com>
2025-05-22 11:52:40 -0400

    first commit

---
+++ src/db/schema.ts
@@ -0,0 +1,105 @@
+import { date, integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+
+export const academicGrade = pgTable("academic_grade", {
+  id: text("id").primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id),
+  academicTaskId: text("academic_task_id")
+    .notNull()
+    .references(() => academicTask.id),
+  courseSessionId: text("course_session_id")
+    .notNull()
+    .references(() => courseSession.id),
+  gradeType: text({ enum: ["letter", "percentage"] }).default("percentage"),
+  percentageGrade: integer("percentage_grade").notNull(),
+  letterGrade: text({
+    enum: [
+      "a+",
+      "a",
+      "a-",
+      "b+",
+      "b",
+      "b-",
+      "c+",
+      "c",
+      "c-",
+      "d+",
+      "d",
+      "d-",
+      "f",
+    ],
+  }).notNull(),
+});
+
+export const academicTask = pgTable("academic_task", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  description: text("description"),
+  taskType: text({ enum: ["assignment", "exam", "other"] }).default(
+    "assignment"
+  ),
+  dueDate: date("due_date", { mode: "date" }),
+  courseSessionId: text("course_session_id")
+    .notNull()
+    .references(() => courseSession.id),
+});
+
+export const course = pgTable("course", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  course_code: text("course_code").notNull(),
+});
+
+export const courseSession = pgTable("course_session", {
+  id: text("id").primaryKey(),
+  courseId: text("course_id")
+    .notNull()
+    .references(() => course.id),
+  instructorId: text("instructor_id")
+    .notNull()
+    .references(() => user.id),
+  sessionStart: date("session_start", { mode: "date" }).notNull(),
+  sessionEnd: date("session_end", { mode: "date" }).notNull(),
+});
+
+export const roster = pgTable("roster", {
+  id: text("id").primaryKey(),
+  studentId: text("student_id")
+    .notNull()
+    .references(() => user.id),
+  courseSessionId: text("course_session_id")
+    .notNull()
+    .references(() => courseSession.id),
+});
+
+export const session = pgTable("session", {
+  id: text("id").primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id),
+  expiresAt: timestamp("expires_at", {
+    withTimezone: true,
+    mode: "date",
+  }).notNull(),
+});
+
+export const user = pgTable("user", {
+  id: text("id").primaryKey(),
+  firstName: text("first_name").notNull(),
+  lastName: text("last_name").notNull(),
+  email: text("email").notNull().unique(),
+  role: text({ enum: ["admin", "teacher", "student"] })
+    .notNull()
+    .default("student"),
+  hashedPassword: text("password_hash").notNull(),
+  dob: date("dob", { mode: "date" }).notNull(),
+});
+
+export type AcademicGrade = typeof academicGrade.$inferSelect;
+export type AcademicTask = typeof academicTask.$inferSelect;
+export type Course = typeof course.$inferSelect;
+export type CourseSession = typeof courseSession.$inferSelect;
+export type Roster = typeof roster.$inferSelect;
+export type Session = typeof session.$inferSelect;
+export type User = typeof user.$inferSelect;
