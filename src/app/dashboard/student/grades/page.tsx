9e9253a3b00fe959c4c39febe9215a6dbd230657
David Eastmond <davebuck1981@gmail.com>
2025-06-04 15:41:25 -0400

    grades main front end component and overview component

--- src/app/dashboard/student/grades/page.tsx
+++ src/app/dashboard/student/grades/page.tsx
@@ -1,12 +1,11 @@
+import { GradesOverviewComponent } from "@/components/grade-components/Grades-overview";
 import { GradesNavMain } from "@/components/nav/student/Grades-nav-main";
 
 export default function StudentGradesPage() {
   return (
     <div>
       <GradesNavMain />
-      <h1 className="text-3xl font-bold">Grades</h1>
-
-      <p>Grades will be displayed here.</p>
+      <GradesOverviewComponent />
     </div>
   );
 }
