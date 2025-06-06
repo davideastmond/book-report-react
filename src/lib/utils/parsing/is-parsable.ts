ecadcb1b584d9f019176c11c2437e1faf4dbed52
David Eastmond <davebuck1981@gmail.com>
2025-06-06 10:23:30 -0400

    installed vitest and wrote isParsable unit test

--- src/lib/utils/parsing/is-parsable.ts
+++ src/lib/utils/parsing/is-parsable.ts
@@ -1,6 +1,28 @@
 export const isParsable = (lg: string): boolean => {
-  if (!lg || isNaN(parseInt(lg, 10))) {
+  const parsedVal = parseInt(lg, 10);
+  if (!lg || isNaN(parsedVal)) {
     return false;
+  }
+
+  if (!isNaN(parsedVal) && containsLetters(lg)) {
+    return false;
+  }
+
+  if (containsMultipleDecimals(lg)) {
+    return false;
+  }
+  if (lg.length > 2) {
+    if (containsLetters(lg)) {
+      return false;
+    }
   }
   return true;
 };
+
+function containsLetters(str: string) {
+  return /[A-Za-z]/.test(str);
+}
+
+function containsMultipleDecimals(str: string) {
+  return /\..*\./.test(str);
+}
