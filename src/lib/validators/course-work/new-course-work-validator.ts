c8ae5bf6c560b1631c4184ff8f390c02c999c67a
David Eastmond <davebuck1981@gmail.com>
2025-06-08 08:32:25 -0400

    added some new course work types - updated validator

--- src/lib/validators/course-work/new-course-work-validator.ts
+++ src/lib/validators/course-work/new-course-work-validator.ts
@@ -6,7 +6,16 @@ export const newCourseWorkValidator = z.object({
     .min(1, "Title is required")
     .max(200, "Title must be less than 200 characters"),
   description: z.string().optional(),
-  taskType: z.enum(["assignment", "exam", "other"]).default("assignment"),
+  taskType: z
+    .enum([
+      "assignment",
+      "exam",
+      "essay",
+      "presentation",
+      "research_paper",
+      "other",
+    ])
+    .default("assignment"),
   dueDate: z.string().date(),
   gradeValueType: z.enum(["p", "l"]).default("p"),
 });
