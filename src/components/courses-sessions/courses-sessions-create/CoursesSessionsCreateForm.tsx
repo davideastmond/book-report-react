d4523b2f60cb3743df8a9fa3070fc8be9f19e17a
David Eastmond <davebuck1981@gmail.com>
2025-07-01 13:47:10 -0400

    fix label

--- src/components/courses-sessions/courses-sessions-create/CoursesSessionsCreateForm.tsx
+++ src/components/courses-sessions/courses-sessions-create/CoursesSessionsCreateForm.tsx
@@ -65,7 +65,7 @@ export function CoursesSessionsCreateForm() {
   return (
     <div className="p-4">
       <h1 className="text-2xl text-center mb-4 font-bold">
-        Create Class Session
+        Create Course Session
       </h1>
 
       <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
@@ -169,7 +169,7 @@ export function CoursesSessionsCreateForm() {
           type="submit"
           className="flatStyle inline-flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
         >
-          Create Class Session
+          Create Course Session
         </button>
       </form>
     </div>
