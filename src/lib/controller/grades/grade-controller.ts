63ffcea3cef305ee90eb828c635ea8681ccd962b
David Eastmond <davebuck1981@gmail.com>
2025-07-03 19:33:52 -0400

    update the grade-controller query

--- src/lib/controller/grades/grade-controller.ts
+++ src/lib/controller/grades/grade-controller.ts
@@ -8,12 +8,72 @@ import {
   roster,
   user,
 } from "@/db/schema";
-import { RawGradeReportData } from "@/lib/types/grading/definitions";
+import { CourseGradingStats } from "@/lib/types/grading/stats/definition";
+import { RawGradeReportData } from "@/lib/types/grading/student/definitions";
 import { and, eq, gte, lte } from "drizzle-orm";
 import { alias } from "drizzle-orm/pg-core";
 
 export const GradeController = {
-  getRawGradeReportData: async ({
+  getRawDataForCourseSessionById: async (
+    courseSessionId: string
+  ): Promise<CourseGradingStats[]> => {
+    const data: CourseGradingStats[] = await db
+      .select({
+        academicTaskId: academicTask.id,
+        academicTaskName: academicTask.name,
+        academicTaskType: academicTask.taskType,
+        courseSessionId: courseSession.id,
+        gradeWeightId: gradeWeight.id,
+        gradeWeightName: gradeWeight.name,
+        gradeWeightPercentage: gradeWeight.percentage,
+        percentageGrade: academicGrade.percentageGrade,
+        sessionStart: courseSession.sessionStart,
+        sessionEnd: courseSession.sessionEnd,
+        isCourseCompleted: courseSession.isCompleted,
+        courseName: course.name,
+        courseCode: course.course_code,
+        studentId: roster.studentId,
+        studentFirstName: user.firstName,
+        studentLastName: user.lastName,
+        studentGender: user.gender,
+        studentDob: user.dob,
+      })
+      .from(roster)
+      .where(eq(roster.courseSessionId, courseSessionId))
+      .innerJoin(user, eq(user.id, roster.studentId))
+      .innerJoin(
+        courseSession,
+        and(
+          eq(courseSession.id, courseSessionId),
+          eq(courseSession.isCompleted, true)
+        )
+      )
+      .innerJoin(
+        academicGrade,
+        and(
+          eq(academicGrade.userId, user.id),
+          eq(academicGrade.courseSessionId, courseSessionId)
+        )
+      )
+      .innerJoin(
+        academicTask,
+        and(
+          eq(academicTask.id, academicGrade.academicTaskId),
+          eq(academicTask.courseSessionId, courseSessionId)
+        )
+      )
+      .innerJoin(
+        gradeWeight,
+        and(
+          eq(gradeWeight.courseSessionId, courseSessionId),
+          eq(gradeWeight.id, academicTask.gradeWeightId)
+        )
+      )
+      .innerJoin(course, eq(course.id, courseSession.courseId));
+
+    return data;
+  },
+  getRawGradeReportDataByDateRange: async ({
     studentId,
     startDate,
     endDate = new Date(),
