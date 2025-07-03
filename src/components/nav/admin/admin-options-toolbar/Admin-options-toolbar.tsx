6fd81504eabb07f8d3a90eda976543f1dc24c920
David Eastmond <davebuck1981@gmail.com>
2025-07-03 19:39:19 -0400

    admin-options-toolbar created

---
+++ src/components/nav/admin/admin-options-toolbar/Admin-options-toolbar.tsx
@@ -0,0 +1,16 @@
+export function AdminOptionsToolbar() {
+  return (
+    <div className="bg-green-900 p-1 text-sm">
+      <ul className="flex justify-start gap-12">
+        <li>
+          <a
+            href={`/dashboard/admin/completed-courses-summary`}
+            className="text-white hover:text-gray-300"
+          >
+            Completed Courses Summary
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
