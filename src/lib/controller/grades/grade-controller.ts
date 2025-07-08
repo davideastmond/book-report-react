c054e572a73186854d3f1db47b8f1c52861a4304
David Eastmond <davebuck1981@gmail.com>
2025-07-08 13:45:11 -0400

    feat: add getRawGradeReport method to GradeController for fetching detailed grade reports

--- src/lib/controller/grades/grade-controller.ts
+++ src/lib/controller/grades/grade-controller.ts
@@ -73,6 +73,49 @@ export const GradeController = {
 
     return data;
   },
+  getRawGradeReport: async ({
+    studentId,
+  }: {
+    studentId: string;
+  }): Promise<RawGradeReportData[]> => {
+    const aliasedUser = alias(user, "alias_user_table");
+    return db
+      .select({
+        academicTaskId: academicTask.id,
+        academicTaskName: academicTask.name,
+        academicTaskType: academicTask.taskType,
+        courseSessionId: courseSession.id,
+        studentId: roster.studentId,
+        studentFirstName: user.firstName,
+        studentLastName: user.lastName,
+        gradeWeightId: gradeWeight.id,
+        gradeWeightName: gradeWeight.name,
+        gradeWeightPercentage: gradeWeight.percentage,
+        percentageGrade: academicGrade.percentageGrade,
+        instructorFirstName: aliasedUser.firstName,
+        instructorLastName: aliasedUser.lastName,
+        sessionStart: courseSession.sessionStart,
+        sessionEnd: courseSession.sessionEnd,
+        isCourseCompleted: courseSession.isCompleted,
+        courseName: course.name,
+        courseCode: course.course_code,
+      })
+      .from(roster)
+      .where(eq(roster.studentId, studentId))
+      .innerJoin(courseSession, eq(courseSession.id, roster.courseSessionId))
+      .innerJoin(course, eq(course.id, courseSession.courseId))
+      .innerJoin(
+        academicTask,
+        eq(academicTask.courseSessionId, courseSession.id)
+      )
+      .innerJoin(
+        academicGrade,
+        eq(academicGrade.academicTaskId, academicTask.id)
+      )
+      .innerJoin(gradeWeight, eq(gradeWeight.id, academicTask.gradeWeightId))
+      .innerJoin(user, eq(user.id, roster.studentId))
+      .innerJoin(aliasedUser, eq(aliasedUser.id, courseSession.instructorId));
+  },
   getRawGradeReportDataByDateRange: async ({
     studentId,
     startDate,
