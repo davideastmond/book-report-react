2deca47f1219c0067ebdea4c01ac79f664260f3f
David Eastmond <davebuck1981@gmail.com>
2025-07-08 18:29:48 -0400

    feat: refactor UserQueryPage to improve data fetching and final grade display

--- src/components/user-query-page/User-query-page.tsx
+++ src/components/user-query-page/User-query-page.tsx
@@ -5,7 +5,7 @@ import {
   UserSummaryData,
 } from "@/lib/types/admin-data/admin-student-data-api-response";
 import { GradeSummaryData } from "@/lib/types/grading/student/definitions";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { Spinner } from "../spinner/Spinner";
 import { UserSearch } from "../user-search/User-Search";
 
@@ -26,32 +26,38 @@ export function UserQueryPage() {
 
   const fetchData = async (userId: string) => {
     setIsBusy(true);
-    const data = await UserClient.getAdminUserData(userId);
-    setStudentInfo(data.studentData);
-    setCourseHistoryInfo(data.coursesData);
-    setGradeSummaryData(data.gradesData);
-    setIsBusy(false);
+    try {
+      const data = await UserClient.getAdminUserData(userId);
+      setStudentInfo(data.studentData);
+      setCourseHistoryInfo(data.coursesData);
+      setGradeSummaryData(data.gradesData);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    } finally {
+      setIsBusy(false);
+    }
   };
 
-  const getFinalGrade = (courseSessionId: string): number | string => {
-    if (!gradeSummaryData) return "N/A";
+  const getFinalGrade = useCallback(
+    (courseSessionId: string) => {
+      if (!gradeSummaryData) return "N/A";
 
-    const foundData = gradeSummaryData.find(
-      (entry) => entry.courseSessionId === courseSessionId
-    );
+      const foundData = gradeSummaryData.find(
+        (entry) => entry.courseSessionId === courseSessionId
+      );
 
-    if (!foundData) return "N/A";
-    return foundData.coursePercentageAverage?.toFixed(2) as string;
-  };
+      if (!foundData) return "N/A";
+      return foundData.coursePercentageAverage?.toFixed(2) as string;
+    },
+    [gradeSummaryData]
+  );
 
   return (
     <div className="flex flex-col h-full">
       <div>
         <UserSearch onUserSelect={handleUserSelected} />
       </div>
-      <table>
-        <thead></thead>
-      </table>
+
       {isBusy && <Spinner />}
       {/* We want to show details after a student is selected */}
       {studentInfo && (
@@ -108,7 +114,11 @@ export function UserQueryPage() {
                   {new Date(course.sessionEnd.toString()).toLocaleDateString()}
                 </td>
                 <td>{course.isCompleted ? "Yes" : "No"}</td>
-                <td>{getFinalGrade(course.courseSessionId)}</td>
+                <td
+                  className={getGradeCSS(getFinalGrade(course.courseSessionId))}
+                >
+                  {getFinalGrade(course.courseSessionId)}
+                </td>
               </tr>
             ))}
           </tbody>
@@ -116,4 +126,16 @@ export function UserQueryPage() {
       )}
     </div>
   );
+}
+
+function getGradeCSS(input: number | string): string {
+  const baseCssClass = "font-thin ";
+  let grade = parseFloat(input as string);
+  if (isNaN(grade)) {
+    return baseCssClass;
+  }
+
+  if (grade >= 80) return baseCssClass + "text-green-500";
+  if (grade >= 70) return baseCssClass + "text-orange-500";
+  return baseCssClass + "text-red-500";
 }
