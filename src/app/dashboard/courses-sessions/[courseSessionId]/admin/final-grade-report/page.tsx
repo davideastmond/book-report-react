d055c6ca011ffe8e8abd21eaf622ca75bc9c4f91
David Eastmond <davebuck1981@gmail.com>
2025-07-09 11:22:21 -0400

    feat: integrate CourseSessionsNavToolbar into final grade report page

--- src/app/dashboard/courses-sessions/[courseSessionId]/admin/final-grade-report/page.tsx
+++ src/app/dashboard/courses-sessions/[courseSessionId]/admin/final-grade-report/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { CourseSessionClient } from "@/clients/course-session-client";
 import { ClassListFinalGradeTable } from "@/components/class-list-final-grade-table/Class-list-final-grade-table";
+import { CourseSessionsNavToolbar } from "@/components/nav/admin/course-sessions-nav-toolbar/Course-sessions-nav-toolbar";
+
 import { SummarizedData } from "@/lib/controller/grades/calculations/definitions";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -23,16 +25,26 @@ export default function CourseSessionFinalReportPage() {
     );
     setFinalGradeReport(data);
   };
+
   return (
-    <div className="flex flex-col items-center justify-center h-full">
-      <h1 className="text-2xl font-bold mb-4">Final Grade Report</h1>
-      <h2 className="text-lg font-semibold">
-        {finalGradeReport?.courseData.courseName} (
-        {finalGradeReport?.courseData.courseCode})
-      </h2>
-      {finalGradeReport && (
-        <ClassListFinalGradeTable data={finalGradeReport.report} />
+    <>
+      <CourseSessionsNavToolbar courseSessionId={params.courseSessionId} />
+      {!finalGradeReport || finalGradeReport.report.length === 0 ? (
+        <div className="flex flex-col items-center justify-center h-full">
+          <p className="text-lg">No final grade report available.</p>
+        </div>
+      ) : (
+        <div className="flex flex-col items-center justify-center h-full">
+          <h1 className="text-2xl font-bold mb-4">Final Grade Report</h1>
+          <h2 className="text-lg font-semibold">
+            {finalGradeReport?.courseData.courseName} (
+            {finalGradeReport?.courseData.courseCode})
+          </h2>
+          {finalGradeReport && (
+            <ClassListFinalGradeTable data={finalGradeReport.report} />
+          )}
+        </div>
       )}
-    </div>
+    </>
   );
 }
