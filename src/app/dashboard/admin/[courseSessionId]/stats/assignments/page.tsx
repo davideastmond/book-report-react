9f28f6214c77f52f16f3932857fb925a9ac659cd
David Eastmond <davebuck1981@gmail.com>
2025-07-11 12:34:16 -0400

    feat: create AssignmentsOverviewPage component for displaying assignment stats

---
+++ src/app/dashboard/admin/[courseSessionId]/stats/assignments/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { AssignmentsOverview } from "@/components/assignments-overview/Assignments-overview";
+import { useAdminAuthorized } from "app/hooks/use-admin-authorized";
+import { useParams } from "next/navigation";
+
+export default function AssignmentsOverviewPage() {
+  const { isAdminAuthorized } = useAdminAuthorized();
+  const params = useParams<{ courseSessionId: string }>();
+
+  if (!isAdminAuthorized) {
+    return (
+      <div className="flex flex-col items-center justify-center h-full">
+        <p className="text-lg">You do not have permission to view this page.</p>
+      </div>
+    );
+  }
+
+  return <AssignmentsOverview courseSessionId={params.courseSessionId} />;
+}
