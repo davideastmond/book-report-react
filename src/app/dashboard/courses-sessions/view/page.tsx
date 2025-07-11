7acd764a88c5d6e52ef7f303243f5d681faa784b
David Eastmond <davebuck1981@gmail.com>
2025-07-11 17:13:18 -0400

    refactor: replace CourseSessionsNavToolbar with isAdminAuthorized for role-based rendering in CourseSessionPage

--- src/app/dashboard/courses-sessions/view/page.tsx
+++ src/app/dashboard/courses-sessions/view/page.tsx
@@ -3,7 +3,6 @@
 
 import { CourseSessionClient } from "@/clients/course-session-client";
 import { CoursesSessionsList } from "@/components/courses-sessions/courses-sessions-list/Courses-sessions-list";
-import { CourseSessionsNavToolbar } from "@/components/nav/admin/course-sessions-nav-toolbar/Course-sessions-nav-toolbar";
 import { Spinner } from "@/components/spinner/Spinner";
 import { StudentList } from "@/components/student-list/Student-list";
 import { UserSearch } from "@/components/user-search/User-Search";
@@ -12,6 +11,7 @@ import {
   EnrolledStudent,
 } from "@/lib/types/db/course-session-info";
 import { useAdmin } from "app/hooks/use-admin";
+import { useAdminAuthorized } from "app/hooks/use-admin-authorized";
 import { useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -34,6 +34,8 @@ export default function CourseSessionPage() {
   const { isAdminEditable } = useAdmin(
     courseSession?.courseSessionId as string
   );
+
+  const { isAdminAuthorized } = useAdminAuthorized();
   useEffect(() => {
     fetchCourseSessionById();
   }, []);
@@ -113,10 +115,6 @@ export default function CourseSessionPage() {
   }
   return (
     <div>
-      {["admin", "teacher"].includes(session?.user?.role as string) &&
-        courseSessionId && (
-          <CourseSessionsNavToolbar courseSessionId={courseSessionId} />
-        )}
       <h1 className="text-3xl py-4">Session Details</h1>
       {courseSession.description && <p>{courseSession.description}</p>}
       <CoursesSessionsList
@@ -126,7 +124,7 @@ export default function CourseSessionPage() {
       {courseSession.isCompleted && (
         <p className="text-amber-300 my-4">Course session completed.</p>
       )}
-      {["admin", "teacher"].includes(session?.user?.role as string) && (
+      {isAdminAuthorized && (
         <div className="mt-10">
           <h3 className="text-2xl">Student Roster</h3>
           <StudentList
@@ -141,7 +139,7 @@ export default function CourseSessionPage() {
           />
         </div>
       )}
-      {["student"].includes(session?.user?.role as string) && (
+      {!isAdminAuthorized && (
         <div className="flex justify-end px-4 mt-4">
           {!isEnrolled && !courseSession.isCompleted && (
             <button
