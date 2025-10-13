3b2599098f43518b503764d2a915347598c8bb7c
David Eastmond <davebuck1981@gmail.com>
2025-10-13 15:49:01 -0400

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import { NavBar } from "@/components/nav/NavBar";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Suspense } from "react";
 import "./globals.css";
 import { NextAuthProvider } from "./providers/NextAuth";
 
@@ -30,7 +30,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <NextAuthProvider>
-          <Suspense>{children}</Suspense>
+          <NavBar />
+          {children}
         </NextAuthProvider>
       </body>
     </html>
