5a7c1c80f59ae337c873adce394cbc6ddb2eb50b
David Eastmond <davebuck1981@gmail.com>
2025-10-22 18:33:01 -0400

    feat: replace table with Card component for course grade summary display

--- src/components/grading-table/student/course/Course-grade-summary-table.tsx
+++ src/components/grading-table/student/course/Course-grade-summary-table.tsx
@@ -3,6 +3,7 @@ The responsibility of this component is to display a table for a course, with th
 */
 
 import { GradeSummaryData } from "@/lib/types/grading/student/definitions";
+import { Card, CardBody } from "@heroui/react";
 
 export function CourseGradeSummaryTable({
   gradeSummaryData,
@@ -10,39 +11,42 @@ export function CourseGradeSummaryTable({
   gradeSummaryData: GradeSummaryData;
 }) {
   return (
-    <table className="table-fixed w-full">
-      <thead className="text-left min-w-[200px]">
-        <tr className="border bg-slate-400/10">
-          <th>Course Code</th>
-          <th>Course Name</th>
-          <th>Instr.</th>
-          <th>Session Start</th>
-          <th>Session End</th>
-          <th>Student Name</th>
-          <th>Grade</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>{gradeSummaryData.courseCode}</td>
-          <td>{gradeSummaryData.courseName}</td>
-          <td>
+    <Card>
+      <CardBody>
+        <p className="font-bold">
+          Course:
+          <span className="font-thin">
+            {gradeSummaryData.courseCode} - {gradeSummaryData.courseName}
+          </span>
+        </p>
+        <p className="font-bold">
+          Instructor:
+          <span className="font-thin">
             {gradeSummaryData.instructorLastName}{" "}
             {gradeSummaryData.instructorFirstName?.slice(0, 1)}
-          </td>
-          <td>
-            {new Date(gradeSummaryData.sessionStart!).toLocaleDateString()}
-          </td>
-          <td>{new Date(gradeSummaryData.sessionEnd!).toLocaleDateString()}</td>
-          <td>
-            {gradeSummaryData.studentLastName}{" "}
+          </span>
+        </p>
+        <p className="font-bold">
+          Session:
+          <span className="font-thin">
+            {new Date(gradeSummaryData.sessionStart!).toLocaleDateString()} -{" "}
+            {new Date(gradeSummaryData.sessionEnd!).toLocaleDateString()}
+          </span>
+        </p>
+        <p className="font-bold">
+          Student:
+          <span className="font-thin">
+            {gradeSummaryData.studentLastName},{" "}
             {gradeSummaryData.studentFirstName?.slice(0, 1)}
-          </td>
-          <td className="text-blue-500">
+          </span>
+        </p>
+        <p className="font-bold">
+          Grade:
+          <span className="text-blue-500">
             {gradeSummaryData.coursePercentageAverage}
-          </td>
-        </tr>
-      </tbody>
-    </table>
+          </span>
+        </p>
+      </CardBody>
+    </Card>
   );
 }
