7754011292b4d5741d3044d6a6330154622fdb82
David Eastmond <davebuck1981@gmail.com>
2025-10-23 14:22:30 -0400

    refactor: remove unused "use client" directive and optimize CoursesSessionsList component

--- src/components/courses-sessions/courses-sessions-list/Courses-sessions-list.tsx
+++ src/components/courses-sessions/courses-sessions-list/Courses-sessions-list.tsx
@@ -1,7 +1,5 @@
-"use client";
 import { CourseSessionsAPIResponse } from "@/lib/types/db/course-session-info";
-import { Card, CardBody, CardHeader, Divider } from "@heroui/react";
-import { useRouter } from "next/navigation";
+import { Card, CardBody, CardHeader, Divider, Link } from "@heroui/react";
 
 type CoursesSessionsListProps = {
   coursesSessions?: CourseSessionsAPIResponse;
@@ -17,7 +15,6 @@ export function CoursesSessionsList({
   linkable,
   enrolled,
 }: CoursesSessionsListProps) {
-  const router = useRouter();
   if (!coursesSessions || coursesSessions.length === 0) {
     return <p>No course sessions found.</p>;
   }
@@ -31,65 +28,69 @@ export function CoursesSessionsList({
             return 0;
           })
           .map((session) => (
-            <Card
-              className={`bg-gray-900 p-3 rounded-md mb-2 w-full ${
-                linkable ? "cursor-pointer hover:shadow-lg" : ""
-              }`}
-              key={session.courseId}
+            <Link
+              href={
+                linkable
+                  ? `/dashboard/courses-sessions/view?id=${session.courseSessionId}`
+                  : "#"
+              }
+              key={session.courseSessionId}
+              data-testid="course-session-card-body"
             >
-              <CardHeader className="flex gap-3">
-                <div className="flex flex-col">
-                  <p className="text-md">{session.courseName}</p>
-                  <p className="text-small text-default-500">
-                    {session.courseCode}
-                  </p>
-                  <p>
-                    <span className="font-bold">Instructor: </span>
-                    {session.instructorLastName?.toLocaleUpperCase()}
-                    {", "}
-                    {session.instructorFirstName
-                      ?.slice(0, 1)
-                      .toLocaleUpperCase()}
-                  </p>
-                </div>
-              </CardHeader>
-              <Divider />
-              <CardBody
-                data-testid="course-session-card-body"
-                onClick={() => {
-                  if (!linkable) return;
-                  router.push(
-                    `/dashboard/courses-sessions/view?id=${session.courseSessionId}`
-                  );
-                }}
+              <Card
+                className={`bg-gray-900 p-3 rounded-md mb-2 w-full ${
+                  linkable ? "cursor-pointer hover:shadow-lg" : ""
+                }`}
+                key={session.courseId}
               >
-                <div className="flex flex-col">
-                  <p className="font-thin">
-                    <span className="font-bold">Starts:</span>{" "}
-                    {new Date(session.sessionStart!).toLocaleDateString()}
-                  </p>
-                  <p className="font-thin">
-                    <span className="font-bold">Ends:</span>{" "}
-                    {new Date(session.sessionEnd!).toLocaleDateString()}
-                  </p>
-                  <p>
-                    <span className="font-bold">Allotment: </span>
-                    <span className=" text-amber-300">
-                      {session.studentAllotment}
-                    </span>
-                  </p>
-                  <p className="text-amber-300">
-                    {session.isCompleted ? "Y" : ""}
-                  </p>
-                  {enrolled && enrolled.show && (
-                    <p>
-                      <span className="font-bold ">Enrolled: </span>
-                      <span className=" text-amber-300">{enrolled.count}</span>
+                <CardHeader className="flex gap-3">
+                  <div className="flex flex-col">
+                    <p className="text-md">{session.courseName}</p>
+                    <p className="text-small text-default-500">
+                      {session.courseCode}
                     </p>
-                  )}
-                </div>
-              </CardBody>
-            </Card>
+                    <p>
+                      <span className="font-bold">Instructor: </span>
+                      {session.instructorLastName?.toLocaleUpperCase()}
+                      {", "}
+                      {session.instructorFirstName
+                        ?.slice(0, 1)
+                        .toLocaleUpperCase()}
+                    </p>
+                  </div>
+                </CardHeader>
+                <Divider />
+                <CardBody>
+                  <div className="flex flex-col">
+                    <p className="font-thin">
+                      <span className="font-bold">Starts:</span>{" "}
+                      {new Date(session.sessionStart!).toLocaleDateString()}
+                    </p>
+                    <p className="font-thin">
+                      <span className="font-bold">Ends:</span>{" "}
+                      {new Date(session.sessionEnd!).toLocaleDateString()}
+                    </p>
+                    <p>
+                      <span className="font-bold">Allotment: </span>
+                      <span className=" text-amber-300">
+                        {session.studentAllotment}
+                      </span>
+                    </p>
+                    <p className="text-amber-300">
+                      {session.isCompleted ? "Y" : ""}
+                    </p>
+                    {enrolled && enrolled.show && (
+                      <p>
+                        <span className="font-bold ">Enrolled: </span>
+                        <span className=" text-amber-300">
+                          {enrolled.count}
+                        </span>
+                      </p>
+                    )}
+                  </div>
+                </CardBody>
+              </Card>
+            </Link>
           ))}
     </div>
   );
