042c11e7e0ea8d11cd64f9fd04e436be06c245ad
David Eastmond <davebuck1981@gmail.com>
2025-10-27 17:37:57 -0400

    test: update completed courses summary page tests for user authorization and redirect behavior

--- src/app/dashboard/admin/completed-courses-summary/page.spec.tsx
+++ src/app/dashboard/admin/completed-courses-summary/page.spec.tsx
@@ -1,5 +1,6 @@
 import { describe, expect, it, vi } from "vitest";
 import CompletedCoursesSummaryPage from "./page";
+
 const serverRedirectMock = vi.fn();
 
 vi.mock("next/navigation", () => ({
@@ -12,13 +13,11 @@ vi.mock("next-auth", () => {
       .fn()
       .mockResolvedValueOnce(null)
       .mockResolvedValueOnce({
-        session: {
-          user: {
-            id: "user-1",
-            email: "user1@example.com",
-            name: "User One",
-            role: "admin",
-          },
+        user: {
+          id: "user-1",
+          email: "user1@example.com",
+          name: "User One",
+          role: "admin",
         },
       }),
   };
@@ -26,12 +25,10 @@ vi.mock("next-auth", () => {
 describe("completed courses summary page tests", () => {
   it("should redirect if user is not authorized to view page", async () => {
     await CompletedCoursesSummaryPage();
-    expect(serverRedirectMock).toHaveBeenCalled();
+    expect(serverRedirectMock).toHaveBeenCalledTimes(1);
   });
-  // it("should render the completed courses summary page for admin user", async () => {
-  //   roleIdx = 0;
-  //   await CompletedCoursesSummaryPage();
-
-  //   expect(serverRedirectMock).not.toHaveBeenCalled();
-  // });
+  it("should render the completed courses summary page for admin user", async () => {
+    await CompletedCoursesSummaryPage();
+    expect(serverRedirectMock).toHaveBeenCalledTimes(2);
+  });
 });
