b4a7ccfeb5925e4f9b0123759bc6fa8762c2328c
David Eastmond <davebuck1981@gmail.com>
2025-10-29 16:34:23 -0400

    feat: add API endpoint for fetching student grades within a date range with authorization checks

--- src/app/api/user/grades/api.ts
+++ src/app/api/user/grades/api.ts
@@ -1,48 +1,39 @@
+"use server";
 import { authOptions } from "@/auth/auth";
 import { calculateGPA } from "@/lib/controller/grades/calculations/gpa-calculator";
 import { StudentGradeCalculator } from "@/lib/controller/grades/calculations/student-grade-calculator";
 import { GradeController } from "@/lib/controller/grades/grade-controller";
-import { validateGradesAPIRequest } from "@/lib/validators/grades/grades-request-validator";
+import { ApiResult } from "@/lib/types/api/api-return-type";
+import { GradeSummaryData } from "@/lib/types/grading/student/definitions";
 
 import { getServerSession } from "next-auth";
-import { NextRequest, NextResponse } from "next/server";
 
 /* 
 Example endpoint
 /api/user/grades?studentId=123&filter=courseSession&startDate=2023-01-01&endDate=2023-12-31
 */
-export async function GET(req: NextRequest) {
+export async function apiGetGradesForStudentWithDateRange(
+  studentId: string,
+  startDate: Date,
+  endDate: Date
+): Promise<ApiResult<{ gradeSummaryData: GradeSummaryData[]; gpa: string }>> {
   const authSession = await getServerSession(authOptions);
   if (!authSession || !authSession.user) {
-    return NextResponse.json(
-      {
-        error: "Unauthorized - no user session found",
-      },
-      { status: 401 }
-    );
+    return {
+      success: false,
+      message: "Unauthorized",
+    };
   }
 
-  const query = req.nextUrl.searchParams;
-
-  try {
-    validateGradesAPIRequest(query);
-  } catch (error) {
-    return NextResponse.json({
-      error: "Invalid query parameters:" + (error as Error).message,
-    });
-  }
-
-  const startDate = query.get("startDate");
-  const endDate = query.get("endDate");
-  const studentId = query.get("studentId");
+  // TODO: do some date validation?
 
   try {
     // Create the raw report data
     const rawReportData =
       await GradeController.getRawGradeReportDataByDateRange({
         studentId: studentId!,
-        startDate: new Date(startDate!),
-        endDate: new Date(endDate || new Date().toISOString()),
+        startDate,
+        endDate: endDate || new Date().toISOString(),
       });
 
     // Based on the raw data, determine the weighted grade
@@ -53,13 +44,15 @@ export async function GET(req: NextRequest) {
     const apiResponse = studentGradeCalculator.collate(calculatedStudentGrades);
 
     const gpa = calculateGPA(calculatedStudentGrades)?.toFixed(1);
-    return NextResponse.json({ data: apiResponse, gpa: gpa });
+
+    return {
+      success: true,
+      data: { gradeSummaryData: apiResponse, gpa: gpa! },
+    };
   } catch (error) {
-    return NextResponse.json(
-      {
-        error: "Grades computation error: " + (error as Error).message,
-      },
-      { status: 400 }
-    );
+    return {
+      success: false,
+      message: "Grades computation error: " + (error as Error).message,
+    };
   }
 }
