7d107491bac23e2fd68001c7885b8706bd865259
David Eastmond <davebuck1981@gmail.com>
2025-10-30 19:01:23 -0400

    refactor: update apiGetCourseGradeAverage function to specify return type

--- src/app/api/courses/sessions/[courseSessionId]/analytics/course-session-grade-average/api.ts
+++ src/app/api/courses/sessions/[courseSessionId]/analytics/course-session-grade-average/api.ts
@@ -2,9 +2,17 @@
 import { CourseSessionGradeCalculator } from "@/lib/controller/grades/calculations/course-session-grade-calculator";
 
 import { GradeController } from "@/lib/controller/grades/grade-controller";
+import { ApiResult } from "@/lib/types/api/api-return-type";
 
 // this route is going to calculate the class average grade for a course session
-export async function apiGetCourseGradeAverage(courseSessionId: string) {
+export async function apiGetCourseGradeAverage(
+  courseSessionId: string
+): Promise<
+  ApiResult<{
+    courseSessionGradeAverage: number;
+    courseSessionId: string;
+  }>
+> {
   try {
     const rawReportData = await GradeController.getRawDataForCourseSessionById(
       courseSessionId
